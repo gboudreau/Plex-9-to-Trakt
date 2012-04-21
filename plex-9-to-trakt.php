a734f918e94368f8bae4ec5c477714a3109e05a9
Guillaume Boudreau <github@danslereseau.com>
2012-04-21 09:31:08 -0300

    - Added movies - Added option to scan multiple sections for TV shows or movies

--- plex-9-to-trakt.php
+++ plex-9-to-trakt.php
@@ -1,11 +1,13 @@
 <?php
 
-// This script adds your shows from Plex 9 to Trakt, including seen/unseen status.
+// This script adds your TV shows & movies from Plex 9 to Trakt, including seen/unseen status.
 
 define('PLEX_URL', 'http://<IP of your Plex 9 media server>:32400');
 define('TRAKT_APIKEY', '<your Trakt.tv API key>');
 define('TRAKT_USERNAME', '<your Trakt.tv username>');
 define('TRAKT_PASSWORD', '<your Trakt.tv password>');
+define('TVSHOWS_SECTIONS', '2'); // Coma-separated value; you can see your sections here: http://<IP of your Plex 9 media server>:32400/library/sections
+define('MOVIES_SECTIONS', '11'); // Coma-separated value; you can see your sections here: http://<IP of your Plex 9 media server>:32400/library/sections
 
 // DO NOT MODIFY ANYTHING BELOW THIS LINE.
 
@@ -14,60 +16,120 @@ echo("\n\n=== Starting the import. This may take some time. ===\n\n");
 ini_set('memory_limit', '512M');
 set_time_limit(6000);
 
-$show_keys = array();
-$shows = array();
+$movies = array();
+foreach (explode(',', MOVIES_SECTIONS) as $movie_section) {
+  echo("=== Looking for Movies in section $movie_section Please wait... ===\n\n");
 
-// Get XML with shows from Plex.
-$xml = simplexml_load_string(file_get_contents(PLEX_URL . '/library/sections/2/all'));
+  // Get XML with movies from Plex.
+  $xml = simplexml_load_string(file_get_contents(PLEX_URL . "/library/sections/$movie_section/all"));
+  
+  foreach ($xml->Video AS $movie)
+  {
+    if ((string) $movie->attributes()->type == 'movie')
+    {
+      $title = (string) $movie->attributes()->title;
+      $movies[$title]->year = (string) $movie->attributes()->year;
+  		if ($movie->attributes()->viewCount)
+			{
+	      $movies[$title]->seen = true;
+				$movies[$title]->lastPlayed = (string) $movie->attributes()->updatedAt;
+			}
+			else
+			{
+				$movies[$title]->seen = false;
+			}
+    }
+  }
+}
+echo("=== Found " . count($movies) ." movies, adding them to Trakt now. ===\n\n");
 
-// Loop through shows and store keys in array.
-foreach ($xml->Directory AS $value)
+$movies_watched = array();
+$movies_unwatched = array();
+foreach ($movies AS $title => $value)
 {
-  $show_keys[] = (string) $value->attributes()->key;
+  echo("Adding movie {$title}\n");
+	unset($movie);
+	$movie->title = (string) $title;
+	$movie->year = (int) $value->year;
+	if ($value->seen) {
+		$movie->plays = 1;
+		$movie->lastPlayed = (int) $value->lastPlayed;
+		$movies_watched[] = $movie;
+	} else {
+		$movies_unwatched[] = $movie;
+	}
+}
+add_movies_watched($movies_watched);
+add_movies_unwatched($movies_unwatched);
+
+function add_movies_watched($movies)
+{
+  $data->movies = $movies;
+  echo curl_post('http://api.trakt.tv/movie/seen/', $data) . "\n";
 }
 
-// Loop through keys and get seasons.
-foreach ($show_keys AS $key)
+function add_movies_unwatched($movies)
 {
-  $xml = simplexml_load_string(file_get_contents(PLEX_URL . $key));
+  $data->movies = $movies;
+  echo curl_post('http://api.trakt.tv/movie/library/', $data) . "\n";
+}
 
+$shows = array();
+foreach (explode(',', TVSHOWS_SECTIONS) as $tvshow_section) {
+  echo("=== Looking for TV Shows in section $tvshow_section. Please wait... ===\n\n");
+  
+  // Get XML with shows from Plex.
+  $xml = simplexml_load_string(file_get_contents(PLEX_URL . "/library/sections/$tvshow_section/all"));
+
+  // Loop through shows and store keys in array.
+  $show_keys = array();
   foreach ($xml->Directory AS $value)
   {
-    if ((string) $value->attributes()->type == 'season')
+    $show_keys[] = (string) $value->attributes()->key;
+  }
+
+  // Loop through keys and get seasons.
+  foreach ($show_keys AS $show_key)
+  {
+    $show_xml = simplexml_load_string(file_get_contents(PLEX_URL . $show_key));
+
+    foreach ($show_xml->Directory AS $season)
     {
-      $title = (string) $xml->attributes()->parentTitle;
-
-      if (!isset($shows[$title]))
+      if ((string) $season->attributes()->type == 'season')
       {
-        $shows[$title]->year = (integer) $xml->attributes()->parentYear;
-        $shows[$title]->seasons = array();
-      }
+        $title = (string) $show_xml->attributes()->parentTitle;
 
-      $key = (string) $value->attributes()->key;
-      $season_no = (string) trim(str_replace('season', '', strtolower($value->attributes()->title)));
-      $shows[$title]->seasons[$season_no] = array();
-
-      // Get the episodes for this season.
-      $episodes_xml = simplexml_load_string(file_get_contents(PLEX_URL . $key));
-
-      foreach ($episodes_xml->Video AS $episode)
-      {
-        if ((string) $episode->attributes()->type == 'episode')
+        if (!isset($shows[$title]))
         {
-          $shows[$title]->seasons[$season_no][(integer) $episode->attributes()->index] = isset($episode->attributes()->viewCount) ? true : false;
+          $shows[$title]->year = (integer) $show_xml->attributes()->parentYear;
+          $shows[$title]->seasons = array();
+        }
+
+        $season_key = (string) $season->attributes()->key;
+        $season_no = (string) $season->attributes()->index;
+        $shows[$title]->seasons[$season_no] = array();
+
+        // Get the episodes for this season.
+        $episodes_xml = simplexml_load_string(file_get_contents(PLEX_URL . $season_key));
+
+        foreach ($episodes_xml->Video AS $episode)
+        {
+          if ((string) $episode->attributes()->type == 'episode')
+          {
+            $shows[$title]->seasons[$season_no][(integer) $episode->attributes()->index] = isset($episode->attributes()->viewCount) ? true : false;
+          }
         }
       }
     }
   }
 }
 
-
 // So now we have all shows with episodes, start year and watch status. Add them to Trakt (as library items if unwatched, otherwise as seen)!
-echo("=== Found " . count($shows) ." shows, adding them to Trakt now. ===\n\n");
+echo("=== Found " . count($shows) ." TV shows, adding them to Trakt now. ===\n\n");
 
 foreach ($shows AS $title => $value)
 {
-  echo("Adding {$title}\n");
+  echo("Adding show {$title}\n");
 
   $data_watched = array();
   $data_unwatched = array();
@@ -76,8 +138,8 @@ foreach ($shows AS $title => $value)
   {
     foreach ($episodes AS $episode => $watched)
     {
-      $ep->season = $season;
-      $ep->episode = $episode;
+      $ep->season = (int) $season;
+      $ep->episode = (int) $episode;
 
       if ($watched)
       {
@@ -94,10 +156,11 @@ foreach ($shows AS $title => $value)
 
   if (count($data_watched) > 0)
   {
-    $data->title = $title;
-    $data->year = $value->year;
+    $data->title = (string) $title;
+    $data->year = (int) $value->year;
     $data->episodes = $data_watched;
 
+    echo "Adding watched TV shows:\n";
     add_show_watched($data);
 
     unset($data);
@@ -109,6 +172,7 @@ foreach ($shows AS $title => $value)
     $data->year = $value->year;
     $data->episodes = $data_unwatched;
 
+    echo "Adding unwatched TV shows:\n";
     add_show_unwatched($data);
 
     unset($data);
@@ -118,21 +182,25 @@ foreach ($shows AS $title => $value)
 // Some function for recurring actions.
 function add_show_watched($data)
 {
-  curl_post('http://api.trakt.tv/show/episode/seen/', $data);
+  echo curl_post('http://api.trakt.tv/show/episode/seen/', $data) . "\n";
 }
 
 function add_show_unwatched($data)
 {
-  curl_post('http://api.trakt.tv/show/episode/library/', $data);
+  echo curl_post('http://api.trakt.tv/show/episode/library/', $data) . "\n";
 }
 
 function curl_post($url, $data)
 {
   set_time_limit(30);
-
+  
   $data->username = TRAKT_USERNAME;
   $data->password = sha1(TRAKT_PASSWORD);
   $data = json_encode($data);
+
+  echo "----------\n";
+  echo "POST $url".TRAKT_APIKEY."\n\n$data\n";
+  echo "----------\n";
 
   $ch = curl_init();
   curl_setopt_array($ch, array(
